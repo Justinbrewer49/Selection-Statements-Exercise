13177ee8390d76775e83b04b5423e3afcdeaac82
justin brewer <justinbrewer49@gmail.com>
2025-04-01 18:44:57 -0400

    exercise 1.0

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            int favNumber = r.Next(1,11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Guess my favorite number between 1 and 10");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                    else if (response > favNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!!!");
+                    }
+                
+               
+                }
+            }
         }
     }
-}
